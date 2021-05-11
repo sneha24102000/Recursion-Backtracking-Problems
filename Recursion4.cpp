30842375f859e57bebdde78277711ba80489478e
Sneha M <67436805+sneha24102000@users.noreply.github.com>
2021-05-11 23:22:42 +0530

    Add files via upload

---
+++ Recursion4.cpp
@@ -0,0 +1,21 @@
+//REVERSE A STRING USING RECURSION
+#include<iostream>
+using namespace std;
+
+void reverse(string s)
+{
+	if(s.length()==0)
+	{
+		return;
+	}
+	
+	string ros = s.substr(1);
+	reverse(ros);
+	cout<<s[0];
+}
+
+int main()
+{
+	reverse("binod");
+	return 0;
+}
