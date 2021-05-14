95bb79559d90612428f385fc31b00bcedf0be385
Sneha M <67436805+sneha24102000@users.noreply.github.com>
2021-05-14 23:31:00 +0530

    Add files via upload

---
+++ Recursion6.cpp
@@ -0,0 +1,25 @@
+//Remove all duplicates from the string
+#include<iostream>
+using namespace std;
+
+string removeDup(string s)
+{
+	if(s.length()==0)
+	{
+		return " ";
+	}
+	
+	char ch= s[0];
+	string ans=removeDup(s.substr(1));
+	if(ch==ans[0])
+	{
+		return ans;
+	}
+	return ch+ans;
+}
+
+int main()
+{
+	cout<<removeDup("aaabbbccceee")<<endl;
+	return 0;
+}
