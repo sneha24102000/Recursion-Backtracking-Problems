b8d70921afdb3f1ec360e8e8e20aa1c15b4aec52
Sneha M <67436805+sneha24102000@users.noreply.github.com>
2021-05-15 12:04:12 +0530

    Add files via upload

---
+++ Recursion9.cpp
@@ -0,0 +1,25 @@
+//Generate substrings of a string
+#include<iostream>
+using namespace std;
+
+void subseq(string s,string ans)
+{
+	if(s.length()==0)
+	{
+		cout<<ans<<endl;
+		return ;
+	}
+	
+	char ch=s[0];
+	string ros=s.substr(1);
+	
+	subseq(ros,ans);
+	subseq(ros,ch+ans);
+}
+
+int main()
+{
+	subseq("ABC"," ");
+	cout<<endl;
+	return 0;
+}
